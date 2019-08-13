9eb013c816981e5d08e16cbaa9b3ac6e241714d1
Jan Müller <janmueller3698@gmail.com>
2019-08-13 19:36:19 +0200

    Switched to Interface for ModelBuilder

--- src/main/java/eu/yeger/minesweeper/Minesweeper.java
+++ src/main/java/eu/yeger/minesweeper/Minesweeper.java
@@ -2,34 +2,25 @@ package eu.yeger.minesweeper;
 
 import eu.yeger.minesweeper.controller.GameController;
 import eu.yeger.minesweeper.model.Game;
+import eu.yeger.minesweeper.model.DefaultModelBuilder;
 import eu.yeger.minesweeper.model.ModelBuilder;
 import eu.yeger.minesweeper.view.ViewBuilder;
 import javafx.scene.Node;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
-@Data
-@AllArgsConstructor
 public class Minesweeper {
 
-    public static final int DEFAULT_WIDTH = 10;
-    public static final int DEFAULT_HEIGHT = 10;
-    public static final int DEFAULT_BOMB_COUNT = 10;
-    public static final int DEFAULT_CELL_SIZE = 40;
-    public static final String DEFAULT_STYLE = "/default.css";
-
     @Builder.Default
-    private int width = DEFAULT_WIDTH;
+    private int width = 10;
     @Builder.Default
-    private int height = DEFAULT_HEIGHT;
+    private int height = 10;
     @Builder.Default
-    private int bombCount = DEFAULT_BOMB_COUNT;
+    private int bombCount = 20;
     @Builder.Default
-    private int cellSize = DEFAULT_CELL_SIZE;
+    private int cellSize = 40;
     @Builder.Default
-    private String style = DEFAULT_STYLE;
+    private String style = "/default.css";
 
     private Runnable onGameWon;
     private Runnable onGameLost;
@@ -38,14 +29,14 @@ public class Minesweeper {
     private ViewBuilder viewBuilder ;
 
     public Node instance() {
-        initCheck();
-        final Game game = modelBuilder.buildGame();
+        defaultBuilderFallback();
+        final Game game = modelBuilder.build();
         new GameController(game, onGameWon, onGameLost);
         return viewBuilder.buildView(game);
     }
 
-    private void initCheck() {
-        if (modelBuilder == null) modelBuilder = new ModelBuilder(width, height, bombCount);
+    private void defaultBuilderFallback() {
+        if (modelBuilder == null) modelBuilder = new DefaultModelBuilder(width, height, bombCount);
         if (viewBuilder == null) viewBuilder = new ViewBuilder(style, cellSize);
     }
 }
